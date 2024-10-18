6d8127f3c4b933ebf0c7aaf206a5a3fd25972199
Ajith Amarnath <kuttenajith1998@gmail.com>
2024-10-18 19:36:18 +0530

    Router with essential files added

---
+++ src/components/Projects.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        projects: [
+          { id: 1, title: 'Portfolio Website' },
+          { id: 2, title: 'E-commerce App' }
+        ]
+      };
+    }
+  };
+  </script>
+  
